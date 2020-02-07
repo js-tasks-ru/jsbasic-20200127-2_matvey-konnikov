73dbba6c3549ad9e6cc623b12d1b6b318da9d8bd
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-02-07 23:48:37 +0300

    решение 1 задачи 3 модуль

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,15 @@
  * @returns {string}
  */
 function showSalary(data, age) {
-}
+    let string = data
+    .filter(item => item.age <= age)
+    .map((item,index,arr) => {
+      if (index === arr.length-1){
+        return `${item.name}, ${item.balance}`
+      }
+      return `${item.name}, ${item.balance}\n`
+    })
+    .join('')
+    console.log(string)
+    return string
+  }
