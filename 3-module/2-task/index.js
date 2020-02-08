181b4ac9f32175995b5e7b0e4bb18c226503f16c
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-02-08 15:51:26 +0300

    решение 2 задачи 3 модуль

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,41 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    let newStr = []
+    let newStr1 = []
+
+    newStr = str.split('')
+
+
+    for (let item of newStr) {
+        let a = ''
+
+        if (isFinite(item) || item === '-' || item === '.') {
+            a += item
+        }
+
+        newStr1.push(a)
+    }
+    newStr =[]
+    let b = ''
+
+    for (let item of newStr1) {
+
+        if (item && item !== ' ') {
+            b += item
+
+            continue
+
+        } else if (!b || b === ' ') {
+            continue
+        }
+
+        newStr.push(+b)
+        b = ''
+    }
+    newStr.push(+b)
+    let c = Math.max(...newStr)
+    let d = Math.min(...newStr)
+
+    return { min: d, max: c }
 }
