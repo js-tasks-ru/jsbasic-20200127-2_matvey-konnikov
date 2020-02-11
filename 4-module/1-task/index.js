7f4b460375b8cd1cea8a872f6d5f48c140600c76
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-02-11 13:01:48 +0300

    решение 1 задачи 4 модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+    let list = document.createElement('ul')
+
+    list.innerHTML += friends
+    .map(item => `<li>${item.firstName} ${item.lastName}</li>`)
+    .join('')
+
+    return list
+
 }
